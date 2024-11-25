7168df661a0c9c8ba75961ec2ecb15cc98728ac3
Adam Adhar <adamadhar28@gmail.com>
2024-11-25 21:12:41 +0100

    [+] ECS : Core folder

---
+++ Assets/_Modules/ECS/Runtime/Core/BuildingType.cs
@@ -0,0 +1,14 @@
+namespace GabE.Module.ECS
+{
+    /// <summary>
+    /// Represents the types of buildings.
+    /// </summary>
+    public enum BuildingType
+    {
+        House,
+        School,
+        Farm,
+        Bookshop,
+        Museum
+    }
+}
---
+++ Assets/_Modules/ECS/Runtime/Core/ResourceType.cs
@@ -0,0 +1,21 @@
+namespace GabE.Module.ECS
+{
+    /// <summary>
+    /// Represents the types of resources available.
+    /// </summary>
+    public enum ResourceType
+    {
+        /// <summary>
+        /// Represents food resource.
+        /// </summary>
+        Food = 0,
+        /// <summary>
+        /// Represents wood resource.
+        /// </summary>
+        Wood = 1,
+        /// <summary>
+        /// Represents stone resource.
+        /// </summary>
+        Stone = 2,
+    }
+}
