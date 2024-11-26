428855e2be8a14565f103deb195d25de1e752eeb
Adam Adhar <adamadhar28@gmail.com>
2024-11-26 04:35:02 +0100

    [BUG] ECS : Person Factory Processor creates X entities   /!\ DO NOT TOUCH

--- Assets/_Modules/ECS/Runtime/Processors/ECS_Processor_PersonFactory.cs
+++ Assets/_Modules/ECS/Runtime/Processors/ECS_Processor_PersonFactory.cs
@@ -1,51 +1,95 @@
-using GabE.Module.ECS;
-using Unity.Burst;
 using Unity.Entities;
+using Unity.Burst;
+using Unity.Mathematics;
 
+using GabE.Module.ECS;
+
+
+/// <summary>
+/// System responsible for creating new person entities.
+/// </summary>
 [BurstCompile]
-//[UpdateInGroup(typeof(UpdateInGroupAttribute))]
-public partial class ECS_Processor_PersonFactory : SystemBase
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public partial struct ECS_Processor_PersonFactory : ISystem
 {
-    #region Fields
+    #region Nested
 
-    private EntityCommandBufferSystem _ecbs;
-
-    #endregion
-
-    #region Lifecycle
-
-    public void OnCreate(ref SystemState state)
+    /// <summary>
+    /// Job to create person entities in parallel.
+    /// </summary>
+    [BurstCompile]
+    private partial struct CreatePersonJob : IJobEntity
     {
-        _ecbs = state.World.GetOrCreateSystemManaged<BeginSimulationEntityCommandBufferSystem>();
-    }
+        /// <summary>
+        /// Parallel writer for entity command buffer.
+        /// </summary>
+        public EntityCommandBuffer.ParallelWriter Buffer;
 
-    public void OnUpdate(ref SystemState state)
-    {
-        var ecb = _ecbs.CreateCommandBuffer();
+        public uint Seed;
 
-        var global = SystemAPI.GetSingletonRW<ECS_Frag_GameGlobal>();
-        if (global.ValueRO.DayCount % 10 == 0)
+        /// <summary>
+        /// Executes the job for each entity in the query.
+        /// </summary>
+        /// <param name="entityInQueryIndex">Index of the entity in the query.</param>
+        public void Execute([EntityIndexInQuery] int entityInQueryIndex)
         {
-            Entity newPerson = ecb.CreateEntity();
-            ecb.AddComponent(newPerson, new ECS_Frag_Person
+            // Random generation compatible [Burst]
+            Random random = new Random(Seed + (uint)entityInQueryIndex);
+
+            Entity newPerson = Buffer.CreateEntity(entityInQueryIndex);
+            Buffer.AddComponent(entityInQueryIndex, newPerson, new ECS_Frag_Person
             {
-                Age = UnityEngine.Random.Range(16, 71),
+                Age = random.NextInt(16, 71),
                 Stamina = 100,
                 IsHappy = true,
                 IsAlive = true
             });
-            ecb.AddComponent(newPerson, new ECS_Frag_Worker
+            Buffer.AddComponent(entityInQueryIndex, newPerson, new ECS_Frag_Worker
             {
                 Work = ECS_Frag_Worker.WorkType.None,
                 IsWorking = false
             });
         }
-
-        _ecbs.AddJobHandleForProducer(state.Dependency);
     }
 
-    protected override void OnUpdate()
+    #endregion
+
+
+    #region Fields 
+
+    uint _seed;
+
+    #endregion
+
+
+    #region Lifecyle
+
+
+    /// <summary>
+    /// System update method. Creates new person entities every 10 game days. //@todo define the creation frequency
+    /// </summary>
+    /// <param name="state">System state.</param>
+    public void OnUpdate(ref SystemState state)
     {
+        if (_seed == 0) 
+            _seed = (uint)math.max(1, (state.WorldUnmanaged.Time.ElapsedTime * 1000));
+
+        EntityCommandBufferSystem _ecbs = state.World.GetOrCreateSystemManaged<BeginSimulationEntityCommandBufferSystem>();
+
+        EntityCommandBuffer.ParallelWriter buffer = _ecbs.CreateCommandBuffer().AsParallelWriter();
+
+        var global = SystemAPI.GetSingletonRW<ECS_Frag_GameGlobal>();
+
+        if (global.ValueRO.DayCount % 10 != 0)
+            return;
+
+        //state.Dependency = new CreatePersonJob
+        //{
+        //    Seed = _seed,
+        //    Buffer = buffer
+        //}.ScheduleParallel(state.Dependency); //Job.Execute() called here
+
+        _ecbs.AddJobHandleForProducer(state.Dependency);
     }
 
     #endregion
