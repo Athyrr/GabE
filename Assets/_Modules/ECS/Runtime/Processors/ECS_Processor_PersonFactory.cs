7ae7e9f071f9f45d5e3c88a15c75c713b6d0f5df
Adam Adhar <adamadhar28@gmail.com>
2024-12-02 01:40:18 +0100

    [Refacto] ECS : PersonFactory ISystem > SystemBase

--- Assets/_Modules/ECS/Runtime/Processors/ECS_Processor_PersonFactory.cs
+++ Assets/_Modules/ECS/Runtime/Processors/ECS_Processor_PersonFactory.cs
@@ -1,136 +1,71 @@
-using Unity.Entities;
-using Unity.Burst;
-using Unity.Mathematics;
-
 using GabE.Module.ECS;
+using Unity.Burst;
+using Unity.Entities;
 using UnityEngine;
-using Unity.Collections;
 
-
-/// <summary>
-/// System responsible for creating new person entities.
-/// </summary>
 [UpdateInGroup(typeof(ECS_Group_Lifecycle))]
-[DisableAutoCreation]
-public partial struct ECS_Processor_PersonFactory : ISystem
+public partial class ECS_Processor_PersonFactory : SystemBase
 {
-    #region Nested
+    private int _lastProcessedDay;
 
-    /// <summary>
-    /// Job to create person entities in parallel.
-    /// </summary>
+    protected override void OnCreate()
+    {
+        _lastProcessedDay = -1;
+    }
+
+    protected override void OnUpdate()
+    {
+        var global = SystemAPI.GetSingleton<ECS_Frag_GameGlobal>();
+        int currentDay = global.DayCount;
+
+        // Check for create
+        if (currentDay % 10 != 0 || currentDay == _lastProcessedDay)
+            return;
+
+        _lastProcessedDay = currentDay;
+        Debug.Log($"ECS : Factory creating a person on Day {currentDay}");
+
+        // Create buffer using ECB system
+        var bufferSystem = World.GetOrCreateSystemManaged<BeginSimulationEntityCommandBufferSystem>();
+        var ecb = bufferSystem.CreateCommandBuffer().AsParallelWriter();
+
+        //@todo generate seed
+        uint seed = (uint)(10);
+
+        // Schedule job and execute
+        Dependency = new CreatePersonJob
+        {
+            CommandBuffer = ecb,
+            Seed = seed
+        }.ScheduleParallel(Dependency);
+
+        // BeginSimulationEntityCommandBufferSystem to auto PlayBack and Dispose Job.
+        bufferSystem.AddJobHandleForProducer(Dependency);
+    }
+
     [BurstCompile]
     private partial struct CreatePersonJob : IJobEntity
     {
-        /// <summary>
-        /// Parallel writer for entity command buffer.
-        /// </summary>
-        //public EntityCommandBuffer.ParallelWriter Buffer;
-
-        [ReadOnly] public EntityCommandBuffer CommandBuffer;
-
-        public EntityManager EntityManager;
-
+        public EntityCommandBuffer.ParallelWriter CommandBuffer;
         public uint Seed;
 
-        /// <summary>
-        /// Executes the job for each entity in the query.
-        /// </summary>
-        /// <param name="entityInQueryIndex">Index of the entity in the query.</param>
         public void Execute([EntityIndexInQuery] int entityInQueryIndex)
         {
-            // Random seed
             Unity.Mathematics.Random random = new Unity.Mathematics.Random(Seed + (uint)entityInQueryIndex);
 
-            //Entity newPerson = CreateEntity(entityInQueryIndex);
-            Entity newPerson = CommandBuffer.CreateEntity();
-            CommandBuffer.AddComponent(newPerson, new ECS_Frag_Person
+            var newPerson = CommandBuffer.CreateEntity(entityInQueryIndex);
+            CommandBuffer.AddComponent(entityInQueryIndex, newPerson, new ECS_Frag_Person
             {
                 Age = random.NextInt(16, 71),
                 Stamina = 100,
                 IsHappy = true,
                 IsAlive = true
             });
-            CommandBuffer.AddComponent(newPerson, new ECS_Frag_Worker
+            CommandBuffer.AddComponent(entityInQueryIndex, newPerson, new ECS_Frag_Worker
             {
                 Work = ECS_Frag_Worker.WorkType.None,
                 IsWorking = false
             });
-
-            CommandBuffer.Playback(EntityManager);
-            CommandBuffer.Dispose();
-
-            Debug.Log("Build Person !");
         }
     }
-
-    #endregion
-
-
-    #region Fields 
-
-    //uint _seed;
-
-    //public EntityCommandBufferSystem _ecbs;
-
-    //private int _processDay = -1;
-
-
-    #endregion
-
-
-
-    #region Lifecyle
-
-    [BurstCompile]
-    public void OnCreate(ref SystemState state)
-    {
-        //base.OnCreate();
-        //_ecbs = World.GetOrCreateSystemManaged<BeginSimulationEntityCommandBufferSystem>();
-
-        Debug.Log("Success get Buffer System");
-
-    }
-
-    /// <summary>
-    /// System update method. Creates new person entities every 10 game days. //@todo define the creation frequency
-    /// </summary>
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
-    {
-        Debug.Log("Update Factory");
-
-        uint _seed = 0;
-        if (_seed == 0)
-            //_seed = (uint)math.max(1, (World.Time.ElapsedTime * 1000));
-            _seed = 10;
-        
-
-
-        //EntityCommandBuffer.ParallelWriter buffer = _ecbs.CreateCommandBuffer().AsParallelWriter();
-        EntityCommandBuffer cb = new EntityCommandBuffer(Allocator.TempJob);
-
-        var global = SystemAPI.GetSingletonRW<ECS_Frag_GameGlobal>();
-
-        int _processDay = 0;
-
-        if (global.ValueRO.DayCount % 10 != 0 || global.ValueRO.DayCount == _processDay)
-            return;
-
-        _processDay = global.ValueRO.DayCount;
-        Debug.Log("Launch Create Job");
-
-        state.Dependency = new CreatePersonJob
-        {
-            CommandBuffer = cb,
-            EntityManager = state.EntityManager,
-            Seed = _seed
-        }.ScheduleParallel(state.Dependency); //Job.Execute() called here
-
-        //_ecbs.AddJobHandleForProducer(Dependency);
-    }
-
-
-
-    #endregion
 }
