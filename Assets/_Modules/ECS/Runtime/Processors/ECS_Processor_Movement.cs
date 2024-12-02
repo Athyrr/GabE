7d4431cc7d5d5bc8d050b3e11374c212e399f812
Adam Adhar <adamadhar28@gmail.com>
2024-12-02 01:39:06 +0100

    [+] ECS : Movement process using Burst + Async

--- Assets/_Modules/ECS/Runtime/Processors/ECS_Processor_Movement.cs
+++ Assets/_Modules/ECS/Runtime/Processors/ECS_Processor_Movement.cs
@@ -1,66 +1,60 @@
 using GabE.Module.ECS;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 
 [UpdateBefore(typeof(ECS_PathFindingSystem))]
 [UpdateInGroup(typeof(ECS_Group_MovementManagement))]
 public partial struct ECS_Processor_Movement : ISystem
 {
-
-    #region Nested 
+    #region Nested
 
     [BurstCompile]
     partial struct MovementJob : IJobEntity
     {
         public float DeltaTime;
+        public float StopDistance;
 
-        public void Execute(ECS_Frag_Position entityPosition, ECS_Frag_Velocity velocity, ECS_Frag_Worker worker, BuildingAspect building)
+        public void Execute(MoverAspect mover, ref ECS_Frag_TargetPosition target)
         {
-            //float3 buildingPosition = float3.zero;
-            //bool foundBuilding = false;
+            float3 currentPosition = mover.Position.ValueRO.Position;
+            float3 direction = math.normalize(target.Position - currentPosition);
 
+            float distance = math.distance(currentPosition, target.Position);
 
-            //if (building.Building.ValueRO.Type == worker.Job)
-            //{
-            //    buildingPosition = b.Position;
-            //    foundBuilding = true;
-            //    break;
-            //}
+            if (distance <= StopDistance)
+                return;
 
-
-            //if (!foundBuilding || !worker.IsWorking)
-            //    return;
-
-            //float3 direction = math.normalize(building.Position - entityPosition.Position);
-
-            //entityPosition.Position += direction * velocity.Value * DeltaTime;
-
-            //if (math.distance(entityPosition.Position, buildingPosition) < 0.1f)
-            //{
-            //    velocity.Value = 0;
-
-            //}
+            mover.Position.ValueRW.Position += direction * mover.Velocity.ValueRO.Value * DeltaTime;
         }
     }
 
-    public readonly partial struct BuildingAspect : IAspect
+    public readonly partial struct MoverAspect : IAspect
     {
         public readonly RefRW<ECS_Frag_Position> Position;
-        public readonly RefRO<ECS_Frag_Building> Building;
+        public readonly RefRO<ECS_Frag_Velocity> Velocity;
     }
 
     #endregion
+
+    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         float deltaTime = SystemAPI.Time.DeltaTime;
 
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+
         var job = new MovementJob
         {
             DeltaTime = deltaTime,
+            StopDistance = 0.1f
         };
 
         state.Dependency = job.ScheduleParallel(state.Dependency);
-    }
 
-  
+        state.Dependency.Complete();
+        ecb.Playback(state.EntityManager);
+        ecb.Dispose();
+    }
 }
