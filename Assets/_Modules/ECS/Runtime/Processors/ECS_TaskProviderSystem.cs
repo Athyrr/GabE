9b778dd6c0db930f14d99e116133457f576a625d
Adam Adhar <adamadhar28@gmail.com>
2024-12-04 02:49:42 +0100

    [+] ECS : Task Provider System + Task Progress System

--- Assets/_Modules/ECS/Runtime/Processors/ECS_TaskProviderSystem.cs
+++ Assets/_Modules/ECS/Runtime/Processors/ECS_TaskProviderSystem.cs
@@ -1,50 +1,87 @@
-using Unity.Burst;
 using Unity.Entities;
+using Unity.Collections;
+using Unity.Burst;
+using GabE.Module.ECS;
 
-namespace GabE.Module.ECS
+[UpdateInGroup(typeof(ECS_LifecycleSystemGroup))]
+public partial struct ECS_TaskProviderSystem : ISystem
 {
-    [UpdateInGroup(typeof(ECS_Group_Lifecycle))]
-    partial struct ECS_Processor_TaskProvider : ISystem
+    private EntityQuery _workerQuery;
+    private EntityQuery _resourceQuery;
+
+    public void OnCreate(ref SystemState state)
     {
-        [BurstCompile]
-        public void OnUpdate(ref SystemState state)
+        // Available workers query 
+        _workerQuery = state.GetEntityQuery(
+            ComponentType.ReadOnly<ECS_WorkerFragment>(),
+            ComponentType.Exclude<ECS_Frag_TargetPosition>()
+        );
+
+        // Resource zones query
+        _resourceQuery = state.GetEntityQuery(
+            ComponentType.ReadOnly<ECS_Frag_Position>(),
+            ComponentType.ReadOnly<ECS_ResourceZoneFragment>()
+        );
+    }
+
+    [BurstCompile]
+    private partial struct TaskProviderJob : IJobEntity
+    {
+        [ReadOnly] public NativeArray<ECS_Frag_Position> ResourcePositions;
+        [ReadOnly] public NativeArray<ECS_ResourceZoneFragment> ResourceTypes;
+        public EntityCommandBuffer.ParallelWriter CommandBuffer;
+
+        public void Execute(Entity entity, ref ECS_WorkerFragment worker)
         {
-            //foreach (var (worker, person) in SystemAPI.Query<RefRW<ECS_Frag_Worker>, RefRO<ECS_Frag_Person>>())
-            //{
-            //    if (worker.ValueRO.IsWorking)
-            //        continue;
+            //if worker holding resources he goes to storage
 
-            //    if (person.ValueRO.Stamina < 40f) // Arbitrary value, Set max value in config
-            //    {
-            //        worker.ValueRW.IsWorking = false;
-            //        continue;
-            //    }
+            if (worker.IsWorking || worker.Work == ECS_WorkerFragment.WorkType.None)
+                return;
 
-            //    worker.ValueRW.IsWorking = true;
+            ResourceType targetType = worker.Work switch
+            {
+                ECS_WorkerFragment.WorkType.FoodHarvester => ResourceType.Food,
+                ECS_WorkerFragment.WorkType.Tiberman => ResourceType.Wood,
+                ECS_WorkerFragment.WorkType.Miner => ResourceType.Stone,
+                _ => ResourceType.Food
+            };
 
-            //    switch (worker.ValueRO.Work)
-            //    {
-            //        case ECS_Frag_Worker.WorkType.FoodHarvester:
-            //            //
-            //            break;
-                        
-            //        case ECS_Frag_Worker.WorkType.Miner:
-            //            //
-            //            break;
-                        
-            //        case ECS_Frag_Worker.WorkType.Tiberman:
-            //            //
-            //            break;
-                        
-            //        case ECS_Frag_Worker.WorkType.Mason:
-            //            //
-            //            break; 
+            for (int i = 0; i < ResourceTypes.Length; i++)
+            {
+                if (ResourceTypes[i].Type == targetType)
+                {
+                    CommandBuffer.AddComponent(entity.Index, entity, new ECS_Frag_TargetPosition
+                    {
+                        Position = ResourcePositions[i].Position
+                    });
 
-            //        case ECS_Frag_Worker.WorkType.Vagabond:
-            //            //
-            //            break;
-            //    }
-            //}
+                    worker.IsWorking = true;
+                    break;
+                }
+            }
         }
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var ecbSystem = state.World.GetExistingSystemManaged<BeginSimulationEntityCommandBufferSystem>();
+        var commandBuffer = ecbSystem.CreateCommandBuffer().AsParallelWriter();
+
+        var resourcePositions = _resourceQuery.ToComponentDataArray<ECS_Frag_Position>(Allocator.TempJob);
+        var resourceTypes = _resourceQuery.ToComponentDataArray<ECS_ResourceZoneFragment>(Allocator.TempJob);
+
+        var job = new TaskProviderJob
+        {
+            ResourcePositions = resourcePositions,
+            ResourceTypes = resourceTypes,
+            CommandBuffer = commandBuffer
+        };
+
+        state.Dependency = job.ScheduleParallel(_workerQuery, state.Dependency);
+
+        ecbSystem.AddJobHandleForProducer(state.Dependency);
+
+        resourcePositions.Dispose();
+        resourceTypes.Dispose();
     }
 }
