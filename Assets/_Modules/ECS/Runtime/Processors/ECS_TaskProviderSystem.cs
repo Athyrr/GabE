34c156238a6eeeb9362f9718cf5a3ad630dd2f36
Adam Adhar <adamadhar28@gmail.com>
2024-12-08 05:01:31 +0100

    [Fixed] ECS : Tasks Provider + Runner

--- Assets/_Modules/ECS/Runtime/Processors/ECS_TaskProviderSystem.cs
+++ Assets/_Modules/ECS/Runtime/Processors/ECS_TaskProviderSystem.cs
@@ -1,26 +1,43 @@
 using Unity.Entities;
 using Unity.Collections;
-using Unity.Burst;
 using GabE.Module.ECS;
+using Unity.Burst;
 
 
 [UpdateInGroup(typeof(ECS_LifecycleSystemGroup))]
+[UpdateAfter(typeof(ECS_TaskProgressionSystem))]
 public partial struct ECS_TaskProviderSystem : ISystem
 {
     private EntityQuery _workerQuery;
     private EntityQuery _resourceQuery;
 
+    private ECS_PositionFragment _storagePosition;
+
 
     [BurstCompile]
-    private partial struct TaskProviderJob : IJobEntity //@todo il en pense quoi David de la boucle dans le job
+    private partial struct ProvideTaskJob : IJobEntity //@todo il en pense quoi David de la boucle dans le job
     {
-        [ReadOnly] public NativeArray<ECS_Frag_Position> ResourcePositions;
+        [ReadOnly] public NativeArray<ECS_PositionFragment> ResourcePositions;
         [ReadOnly] public NativeArray<ECS_ResourceZoneFragment> ResourceTypes;
+
+        [ReadOnly] public ECS_PositionFragment StoragePosition;
+
         public EntityCommandBuffer.ParallelWriter CommandBuffer;
 
-        public void Execute(Entity entity, ref ECS_WorkerFragment worker)
+        public void Execute(Entity entity, [EntityIndexInQuery] int index, ref ECS_WorkerFragment worker)
         {
-            //if worker holding resources he goes to storage postion
+            if (!worker.IsRetrieving && !worker.IsWorking && worker.HoldResourcesAmount >= worker.HoldingCapacity)
+            {
+                CommandBuffer.SetComponent(entity.Index, entity, new ECS_Frag_TargetPosition
+                {
+                    Position = StoragePosition.Position
+                });
+
+                worker.IsRetrieving = true;
+            }
+
+            if (worker.IsRetrieving)
+                return;
 
             if (worker.IsWorking || worker.Work == ECS_WorkerFragment.WorkType.Vagabond)
                 return;
@@ -30,7 +47,8 @@ public partial struct ECS_TaskProviderSystem : ISystem
                 ECS_WorkerFragment.WorkType.FoodHarvester => ResourceType.Food,
                 ECS_WorkerFragment.WorkType.Tiberman => ResourceType.Wood,
                 ECS_WorkerFragment.WorkType.Miner => ResourceType.Stone,
-                _ => ResourceType.Food
+                ECS_WorkerFragment.WorkType.Mason => ResourceType.None,
+                _ => ResourceType.None
             };
 
             for (int i = 0; i < ResourceTypes.Length; i++)
@@ -42,41 +60,60 @@ public partial struct ECS_TaskProviderSystem : ISystem
                         Position = ResourcePositions[i].Position
                     });
 
-                    worker.IsWorking = true;
+                    worker.IsRetrieving = false;
+
                     break;
                 }
             }
         }
     }
 
+
+    //[BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         // Available workers query 
         _workerQuery = state.GetEntityQuery(
-            ComponentType.ReadOnly<ECS_WorkerFragment>(),
-            ComponentType.Exclude<ECS_Frag_TargetPosition>()
+            ComponentType.ReadOnly<ECS_WorkerFragment>()
         );
 
         // Resource zones query
         _resourceQuery = state.GetEntityQuery(
-            ComponentType.ReadOnly<ECS_Frag_Position>(),
+            ComponentType.ReadOnly<ECS_PositionFragment>(),
             ComponentType.ReadOnly<ECS_ResourceZoneFragment>()
         );
+
+        // Get storage position
+        _storagePosition = GetStoragePosition(ref state);
     }
 
-    //[BurstCompile(OptimizeFor = OptimizeFor.Performance)]
+    private ECS_PositionFragment GetStoragePosition(ref SystemState state)
+    {
+        var storageQuery = state.GetEntityQuery(
+            ComponentType.ReadOnly<ECS_ResourceStorageFragment>()
+        );
+
+        Entity storageEntity = storageQuery.GetSingletonEntity();
+        ECS_PositionFragment storagePosition = state.EntityManager.GetComponentData<ECS_PositionFragment>(storageEntity);
+
+        return storagePosition;
+    }
+
+
+    [BurstCompile(OptimizeFor = OptimizeFor.Performance)]
     public void OnUpdate(ref SystemState state)
     {
         EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
         var writter = commandBuffer.AsParallelWriter();
 
-        var resourcePositions = _resourceQuery.ToComponentDataArray<ECS_Frag_Position>(Allocator.TempJob);
+        var resourcePositions = _resourceQuery.ToComponentDataArray<ECS_PositionFragment>(Allocator.TempJob);
         var resourceTypes = _resourceQuery.ToComponentDataArray<ECS_ResourceZoneFragment>(Allocator.TempJob);
 
-        var taskProviderjob = new TaskProviderJob
+        var taskProviderjob = new ProvideTaskJob
         {
             ResourcePositions = resourcePositions,
             ResourceTypes = resourceTypes,
+            StoragePosition = _storagePosition,
             CommandBuffer = writter
         };
 
